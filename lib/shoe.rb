b6a38e6e88beb92444f4769d85d487f206f45ce3
Laura Trager <laura.trager@gmail.com>
2017-11-09 18:44:08 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,6 @@
-#learn spec/02_shoe_spec.rb
-
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
-  def initialize(brand)
-    @brand = brand
-  end
 
   BRANDS = []
 
