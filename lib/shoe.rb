9423d400e542f76821f1341adac3dba154fcd386
Laura Trager <laura.trager@gmail.com>
2017-11-09 18:38:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,6 +4,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRAND = []
+
   def initialize(brand)
     @brand = brand
   end
@@ -11,6 +13,11 @@ class Shoe
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def brand=(brand)
+    @brand = brand
+    BRAND << brand
   end
 
 end
